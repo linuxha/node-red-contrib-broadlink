03d317f928889aeeb8a88d625815a00e99c6f2d4
Neil Cherry <ncherry@linuxha.com>
2017-08-24 10:13:16 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# node-red-contrib-broadlink
+
+Node-red node for controlling Broadlink devices
+
+The node allows you to manage the following devices:
+
+* A1
+* RM Pro
+* SP2
+* MP1
+* S1C
+
+# Author:
+Vladimir Filippov
+
+# Note:
+This is an english translation of the Readme. I'm trying to fix a message I get on my Node-red service about a Fatal error.
+# node-red-contrib-broadlink Readem
+# node-red-contrib-broadlink-vf Readem
---
+++ nodes/SP2.js
@@ -0,0 +1,39 @@
+﻿var Device = require("./Device.js");
+class SP2 extends Device {
+    constructor(host, mac, timeout = 10) {
+        super(host, mac, timeout);
+
+        this.on("payload", (err, payload) => {
+            var param = payload[0];
+            //console.log(payload.join(' '))
+            switch (param) {
+                case 1:
+                    this.emit("data", Boolean(payload[0x4]));
+                    break;
+            }
+        });
+    }
+    set_power(state) {
+        //"""Sets the power state of the smart plug."""
+        var packet = Buffer.alloc(16, 0);
+        packet[0] = 2;
+        packet[4] = state ? 1 : 0;
+        this.sendPacket(0x6a, packet);
+    }
+
+    check_power() {
+        //"""Returns the power state of the smart plug."""
+        var packet = Buffer.alloc(16, 0);
+        packet[0] = 1;
+        this.sendPacket(0x6a, packet);
+        /*
+           err = response[0x22] | (response[0x23] << 8);
+           if(err == 0){
+           aes = AES.new(bytes(this.key), AES.MODE_CBC, bytes(self.iv));
+           payload = aes.decrypt(bytes(response[0x38:]));
+           return bool(payload[0x4]);
+           }
+           */
+    }
+}
+module.exports = SP2;
